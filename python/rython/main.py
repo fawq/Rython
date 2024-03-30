054d2686e6a2d2799c4588d1efc87b95c7f8bbd8
Krystian Krakowski <kkrakowski22@gmail.com>
2024-03-30 13:16:33 +0100

    Add test performance

--- python/rython/main.py
+++ python/rython/main.py
@@ -1,4 +1,21 @@
+from time import perf_counter
 from rython import calc
 
 if __name__ == "__main__":
-    print(calc.sum_as_string(1, 2))
+    NUMBER_OF_LOOPS = 10**7
+
+    t1_start = perf_counter()
+    new_int = calc.NewInt(1)
+    for i in range(NUMBER_OF_LOOPS):
+        new_int.add(0)
+        new_int.mul(1)
+    t1_stop = perf_counter()
+    print(f"Difference: {t1_stop - t1_start}")
+
+    t2_start = perf_counter()
+    base_int = 1
+    for i in range(NUMBER_OF_LOOPS):
+        base_int += 0
+        base_int *= 1
+    t2_stop = perf_counter()
+    print(f"Difference: {t2_stop - t2_start}")
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,77 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction(text_signature = "(a: int, b: int) -> str")]
+#[pyclass]
+struct NewInt {
+    number: i32
+}
+
+#[pymethods]
+impl NewInt {
+    #[new]
+    #[pyo3(text_signature = "(number: int) -> None")]
+    fn new(number: i32) -> Self {
+        Self {number}
+    }
+
+    #[pyo3(text_signature = "($self, second_number: int) -> int")]
+    fn mul(&mut self, second_number: i32) -> PyResult<i32> {
+        self.number *= second_number;
+        Ok(self.number)
+    }
+
+    #[pyo3(text_signature = "($self, second_number: int) -> int")]
+    fn add(&mut self, second_number: i32) -> PyResult<i32> {
+        self.number += second_number;
+        Ok(self.number)
+    }
+    
+    #[pyo3(text_signature = "($self) -> int")]
+    fn get_number(&self) -> PyResult<i32> {
+        Ok(self.number)
+    }
+}
+
+#[pyclass]
+struct NewFloat {
+    number: f64
+}
+
+#[pymethods]
+impl NewFloat {
+    #[new]
+    #[pyo3(text_signature = "(number: float) -> None")]
+    fn new(number: f64) -> Self {
+        Self {number}
+    }
+
+    #[pyo3(text_signature = "($self, second_number: float) -> float")]
+    fn mul(&mut self, second_number: f64) -> PyResult<f64> {
+        self.number *= second_number;
+        Ok(self.number)
+    }
+
+    #[pyo3(text_signature = "($self, second_number: float) -> float")]
+    fn add(&mut self, second_number: f64) -> PyResult<f64> {
+        self.number += second_number;
+        Ok(self.number)
+    }
+
+    #[pyo3(text_signature = "($self) -> float")]
+    fn get_number(&self) -> PyResult<f64> {
+        Ok(self.number)
+    }
+}
+
+#[pyfunction]
+#[pyo3(text_signature = "(a: int, b: int) -> str")]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn calc(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<NewInt>()?;
+    m.add_class::<NewFloat>()?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
